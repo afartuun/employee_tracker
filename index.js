4c60a4023507c55c1e978d6e76cbc4d6aa7dab4f
Fartuun Ali <fartuunali@Fartuuns-MacBook-Pro.local>
2022-08-23 16:46:19 -0500

    adding chosen functions to index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const db = require('./db/connection');
 
 
 //first question prompt
-const choosePrompt = {
+const chooseQuest = {
         type: 'list',
         name: 'options',
         message: 'Please choose what you want to do:',
@@ -12,7 +12,80 @@ const choosePrompt = {
 
 
 async function initial_prompt() {
-    const choose = await inquirer.prompt(choosePrompt);
-
-
+    const choose = await inquirer.prompt(chooseQuest);
 }
+
+const add_deptPrompt = {
+    type: 'input',
+    name: 'addDepartment',
+    message: 'New department name:'
+
+};
+
+async function addDepartment() {
+    const depInput =  await inquirer.prompt(add_dept);
+    const newDept = new Department(depInput.addDepartment);
+
+    const dataID = await db.query(`INSERT INTO deptTable SET depName = ?`, newDept.dep_name);
+    newDept.getId(dataID[0].insertID);
+    console.log(`${newDept.dep_name} department was added!`);
+    return initial_prompt();
+};
+
+const add_emp = {
+    type: 'input',
+    name: 'addEmployee',
+    message: 'New employee name:'
+
+};
+
+async function addEmployee() {
+    const empInput =  inquirer.prompt(add_emp);
+    const newEmp = new Employee(empInput.addEmployee);
+
+    const empID = await db.query(`INSERT INTO emplTable SET empName = ?`, newEmp.emp_name);
+    newEmp.getId(dataID[0].insertID);
+    console.log(`${newEmp.emp_name} employee was added!`);
+    return initial_prompt();
+};
+const department_names = newDept;
+const addRole = [
+    {
+        type: 'input',
+        name: 'role',
+        message: "New job role:"
+    },
+    {
+        type: 'input',
+        name: 'role_salary',
+        message: "Base salary for new role:"
+    },
+    {
+        type: 'list',
+        name: 'role_dept',
+        message: "New role department:",
+        choices: department_names
+    }
+];
+
+function main() {
+    inquirer.prompt(chooseQuest)
+    .then(data => {
+        console.log(data);
+    })
+};
+function chosen(data) {
+    if(data.options === 'Add a department') {
+        prompt(add_deptPrompt).then(data => {
+            console.log(data);
+        })
+    }else if(data.options === 'Add a role') {
+        prompt(addRole).then(data =>{
+            console.log(data);
+        })
+    }else (data.options ==='Add an employee') 
+        prompt(add_emp).then(data => {
+            console.log(data);
+        })
+    }
+    
